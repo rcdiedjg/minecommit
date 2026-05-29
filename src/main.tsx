c919abe43b66e9132ee966aaadc602e55bebfe56
HairlessVillager <hairlessvillager@foxmail.com>
2026-05-30 07:26:17 +0800

    Sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,22 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { TooltipProvider } from "@/components/ui/tooltip.tsx"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar.tsx"
+import { AppSidebar } from "@/components/app-sidebar.tsx"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <TooltipProvider>
+        <SidebarProvider>
+          <AppSidebar />
+          <main>
+            <SidebarTrigger />
+            <App />
+          </main>
+        </SidebarProvider>
+      </TooltipProvider>
     </ThemeProvider>
   </StrictMode>
 )
