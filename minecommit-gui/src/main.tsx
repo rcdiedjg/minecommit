84f08a9d6c0ff989385b711b15f51f4283e409d6
HairlessVillager <hairlessvillager@foxmail.com>
2026-05-30 23:16:17 +0800

    Merge minecommit-gui-frontend subtree

--- minecommit-gui/src/main.tsx
+++ minecommit-gui/src/main.tsx
@@ -1,9 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+import "./index.css"
+import { App } from "./App.tsx"
+import { ThemeProvider } from "@/components/theme-provider.tsx"
+import { TooltipProvider } from "@/components/ui/tooltip.tsx"
+import { SidebarProvider } from "@/components/ui/sidebar.tsx"
+import { AppSidebar } from "@/components/app-sidebar.tsx"
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider>
+      <TooltipProvider>
+        <BrowserRouter>
+          <SidebarProvider defaultOpen={false}>
+            <AppSidebar />
+            <App />
+          </SidebarProvider>
+        </BrowserRouter>
+      </TooltipProvider>
+    </ThemeProvider>
+  </StrictMode>
+)
